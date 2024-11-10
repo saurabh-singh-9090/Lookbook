5a218751c46becf0a0503716bfd2dcd2b9b1d179
Saurabh Singh <saurabhsingh@Saurabhs-MacBook-Air.local>
2024-11-10 19:08:24 +0530

    code refactoring 2

--- src/Lookbook/Components/ProductCards.jsx
+++ src/Lookbook/Components/ProductCards.jsx
@@ -1,10 +1,10 @@
 
-export default function ProductCards({ currentMediaItem }) {
+export default function ProductCards({ currentMediaItem , handleProductClick}) {
     return (
         <div className="product-cards">
             <div className="product-cards-container">
                 {currentMediaItem?.products?.map((product) => (
-                    <div key={product?.id} className="product-card">
+                    <div onClick={handleProductClick} key={product?.id} className="product-card">
                         <p>{product.name}</p>
                         <p className="price">${product.price.toFixed(2)}</p>
                     </div>
